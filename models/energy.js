c248369d97cd8701b4b79d5a5fcc17799a309845
Fady Abdelmohsen <fady.abdelmohsen@gmail.com>
2015-06-14 17:02:53 -0300

    replaced mongojs with mongoose

--- models/energy.js
+++ models/energy.js
@@ -2,25 +2,24 @@ var Mongoose = require('mongoose')
    , Schema = Mongoose.Schema;
 
 Mongoose.model("dataHour", new Schema({
-    date            :  { type: [Date], index: true },
-    time            :  { type: [String], index: true }, 
-    code            :  { type: [String], index: true },
+    date            :  Date,
+    time            :  String, 
+    code            :  String,
     status          :  String,
     value           :  Number
 }, {collection  : "dataHour"} ));
 
 Mongoose.model("dataDaily", new Schema({
-    date            :  { type: [Date], index: true },
-    time            :  { type: [String], index: true }, 
-    code            :  { type: [String], index: true },
+    date            :  Date,
+    time            :  String, 
+    code            :  String,
     status          :  String,
     value           :  Number
 }, {collection  : "dataDaily"} ));
 
 Mongoose.model("dataMonthly", new Schema({
-    month           :  { type: [String], index: true },
-    code            :  { type: [String], index: true }, 
-    year            :  { type: [String], index: true },
+    month           :  String, 
+    code            :  String,
     status          :  String,
     value           :  Number
 }, {collection  : "dataMonthly"})); 
