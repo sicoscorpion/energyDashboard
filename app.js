e9b1f45b07f2031fd13db0fb52edf0cf9f2687c6
Fady Abdelmohsen <fady.abdelmohsen@gmail.com>
2016-02-21 10:29:25 -0400

    Add CORS fix

--- app.js
+++ app.js
@@ -78,6 +78,11 @@ if ('development' == env || 'remote' == env) {
     app.use(passport.initialize());
     app.use(passport.session());
     // app.use(app.router);
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 }
 
 var job1 = new cronJob({
